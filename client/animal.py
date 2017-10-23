4d3b98249cd5ac8e61613ba07f8888d771fc7f81
Mitch Patenaude <dialagrue@gmail.com>
2017-10-23 20:32:19 +0000

    Moved print logic to tree.py (not source controlled) where it belongs

--- client/animal.py
+++ client/animal.py
@@ -126,20 +126,6 @@ class Tree(object):
                     nodes.extend(new_nodes)
             yield depth, seq, node
 
-    def printLeaves(self, verbose=False, depth_first=False):
-        nodes = []
-        nodes.append((1, [], Node.find(1, self._conn)))
-        for depth, seq, node in self.walk(depth_first=depth_first):
-            if not node.yes_node:
-                if verbose:
-                    d = 0
-                    for answer, question in seq:
-                        print('{0}{1}: {2}'.format(' ' * d, question, answer))
-                        d = d+1
-                    print("{0}-> {1}\n".format(' ' * depth, node.node_text))
-                else:
-                    print("{0} {1} {2}".format(depth, ','.join([answer for answer, question in seq]), node.node_text))
-
     def search(self, name):
         for depth, seq, node in self.walk():
             if not node.yes_node and node.node_text.lower().strip() == name.lower().strip():
