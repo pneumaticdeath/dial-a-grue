97da6264fb9f2b81ea8ab7f3b6b883b0dccff2fe
Mitch Patenaude <dialagrue@gmail.com>
2018-04-13 16:43:57 +0000

    Slightly more playable

--- client/modules/Hammurabi.py
+++ client/modules/Hammurabi.py
@@ -15,11 +15,12 @@ INSTANCE_WORDS = [
     'NO',
     'QUIT',
     'NONE', 
+    'ALL',
 ] + mk_upper(text2int.units) + mk_upper(text2int.tens) + mk_upper(text2int.scales)
 
 PRIORITY = 50
 
-def get_number(prompt, mic):
+def get_number(prompt, mic, all_value=None):
     retval = None
     while retval is None:
         print(prompt)
@@ -29,6 +30,8 @@ def get_number(prompt, mic):
             heard = mic.activeListen()
         if heard == 'NONE':
             return 0
+        elif heard == 'ALL' and all_value is not None:
+            return all_value
         try:
             retval = text2int.text2int(heard)
             if 'NO' == get_yes_no('I heard {0}, is that right?'.format(retval), mic):
@@ -56,6 +59,8 @@ def handle(text, mic, profile):
     """
      
     logging.info('Starting the Hammurabi module')
+
+    Hammurabi.reign = 5
     mic.say('Welcome to Hammurabi\'s Babylon.  You will reign for {0} years.'.format(Hammurabi.reign))
 
     game = Hammurabi()
@@ -69,13 +74,20 @@ def handle(text, mic, profile):
         else:
             print('Try again...')
             mic.say('Let\'s try that again!')
+
         print('Feed?')
-        feed = get_number('How many bushels do you wish to feed to your people?', mic)
+        people_fed = get_number('How many people do you want to feed?', mic, game.population)
+        feed = people_fed*game.food_per_person
+
         print('Plant?')
-        plant = get_number('How many acres would you like to plant?', mic)
+        plant = get_number('How many acres would you like to plant?', mic, min(game.acreage, game.population*game.acres_per_person))
+
+        print('Real estate?')
         land = int((game.grain-feed-plant*game.seed_per_acre)/game.land_price)
-        action = 'are able to buy' if land > 0 else 'will need to sell'
-        mic.say('To do that you {0} {1} acres of land'.format(action, abs(land)))
+        if land < 0: land -= 1 # stupid rouding errors
+        action = 'are able to buy at most' if land > 0 else 'will need to sell at least'
+        print('Default land is {0}'.format(land))
+        mic.say('Given that, you {0} {1} acres of land'.format(action, abs(land)))
         answer = get_yes_no('Is that what you\'d like to do?', mic)
         if answer == 'NO':
             print('Buy land?')
@@ -86,19 +98,30 @@ def handle(text, mic, profile):
                 land = -land_sold
             else:
                 land = land_bought
+
+        print('Sanity Check...')
+        land_plantable = min(game.acreage + land, game.population*game.acres_per_person)
+        if plant > land_plantable:
+            print('Not enough land..')
+            mic.say('But you can plant at most {0} acres.'.format(land_plantable))
+            answer = get_yes_no('Is that what you\'d like to do?', mic)
+            if answer == 'YES':
+                plant = land_plantable
+
         print('land: {0}, feed: {1}, plant: {2}'.format(land, feed, plant))
         try:
             game.run(land, feed, plant)
         except ValueError as e:
             print(str(e))
             mic.say(str(e))
+    print(game.status())
     mic.say(game.status())
 
     logging.info('Leaving Hammurabi module')
 
 def isValid(text):
     """
-    Responds to the phrase "Play Animal"
+    Responds to the phrase "Play Hammurabi"
     """
 
     return bool(re.search(r'\bhammurabi\b', text, re.IGNORECASE))
