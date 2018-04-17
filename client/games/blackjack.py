cabc7dc1e38e3ca56b456d51e542d0ec70c20ace
Mitch Patenaude <dialagrue@gmail.com>
2018-04-17 23:38:37 +0000

    Blackjack now has betting

--- client/games/blackjack.py
+++ client/games/blackjack.py
@@ -74,6 +74,10 @@ class Deck(object):
             yield card
 
 class Hand(list):
+    def __init__(self, bet, *args, **kwargs):
+        super(Hand, self).__init__(*args, **kwargs)
+        self.bet = bet
+
     def __str__(self):
         if len(self) == 0:
             return 'an empty hand'
@@ -96,22 +100,54 @@ def _count(hand):
             count -= 10
     return count
 
+class InsufficientFunds(Exception):
+    pass
+
+class Bank(object):
+    def __init__(self, num, initial_balance=100):
+        self.balances = [initial_balance for _ in range(num+1)]
+
+    def withdraw(self, player, amount):
+        if self.balances[player] < amount:
+            raise InsufficientFunds('Insufficient funds')
+        self.balances[player] -= amount
+
+    def deposit(self, player, amount):
+        self.balances[player] += amount
+
+    def balance(self, player):
+        return self.balances[player]
+
+
 class Dealer(object):
-    def __init__(self):
+    def __init__(self, num_players, initial_balance=100, min_bet=10):
         self.deck = Deck()
         self.deck.shuffle()
-        self.hands = None
+        self.num_players = num_players
+        self.initial_balance = initial_balance
+        self.min_bet = min_bet
+        self.bank = Bank(num_players + 1, initial_balance)
+        self.bank.deposit(0, 99*initial_balance) # house has lots of money
+        self.hands = {}
 
-    def deal(self, num_players=1):
-        self.num_players=num_players
+    def deal(self, min_bet=10):
         # hand #0 is the dealer
-        num_hands = num_players + 1
+        num_hands = self.num_players + 1
         if (num_hands * 4) > self.deck.cardsLeft():
             self.deck.shuffle()
-        self.hands = [[Hand()] for _ in range(num_hands)]
+        self.hands = {}
+        for player in range(num_hands):
+            if self.bank.balance(player) >= self.min_bet:
+                self.bank.withdraw(player, self.min_bet)
+                self.bank.deposit(0, self.min_bet)
+                self.hands[player] = [Hand(self.min_bet)]
+
         for c in range(2):
-            for h in range(num_hands):
+            for h in self.hands.keys():
                 self.hands[h][0].append(self.deck.deal())
+
+    def players(self):
+        return sorted(list(filter(lambda x: x != 0, self.hands.keys())))
 
     def hand(self, player, hand=0):
         return self.hands[player][hand]
@@ -128,7 +164,9 @@ class Dealer(object):
         return new_card, self.count(player, hand)
 
     def split(self, player, hand):
-        new_hand = Hand()
+        self.bank.withdraw(player, self.hands[player][hand].bet)
+        self.bank.deposit(0, self.hands[player][hand].bet)
+        new_hand = Hand(self.hands[player][hand].bet)
         new_hand.append(self.hands[player][hand][1])
         self.hands[player][hand][1] = self.deck.deal()
         self.hands[player].append(new_hand)
@@ -179,14 +217,23 @@ plays a game of blackjack with up to 10 players.
 Pass input_func and output_func with appropriate vectors for other implementations.
     '''
 
-    dealer = Dealer()
     output_func('How many players?')
     n = read_int(low_limit=1, high_limit=10, input_func=input_func, output_func=output_func)
+    dealer = Dealer(n)
+    output_func('Everybody starts with ${0}.  Standard bet is ${1}'.format(dealer.initial_balance, dealer.min_bet))
+
     keep_playing = 'yes'
     while keep_playing == 'yes':
         dealer.deal(n)
+        players = dealer.players()
+        if not players:
+            output_func('Everybody is broke!')
+            break
         output_func('Dealer has a face down card and {0}'.format(str(dealer.hand(0)[1])))
-        for player in range(1,n+1):
+        for player in range(1, n+1):
+            if player not in players:
+                output_func('Player {0} has insufficient funds to play this round'.format(player))
+                continue
             hand_num = 0
             while hand_num < dealer.num_hands(player):
                 count = dealer.count(player, hand_num)
@@ -204,18 +251,28 @@ Pass input_func and output_func with appropriate vectors for other implementatio
                         output_func('{0}?'.format(mk_print_list(options, 'or')))
                         ans = read_answer(options, input_func=input_func, output_func=output_func)
                         if ans == 'split':
-                            dealer.split(player, hand_num)
-                            count = dealer.count(player, hand_num)
-                            output_func('{0} hand of player {1} now has {2} with a count of {3}'.format(
-                                ordinal[hand_num + 1].capitalize(),
-                                player,
-                                dealer.hand(player, hand_num),
-                                count))
+                            try:
+                                dealer.split(player, hand_num)
+                                count = dealer.count(player, hand_num)
+                                output_func('{0} hand of player {1} now has {2} with a count of {3}'.format(
+                                    ordinal[hand_num + 1].capitalize(),
+                                    player,
+                                    dealer.hand(player, hand_num),
+                                    count))
+                            except InsufficientFunds:
+                                output_func('Player {0} doesn\'t have sufficient funds to double the bet'.format(player))
                             continue
                         elif ans == 'double':
-                            card, count = dealer.hit(player, hand_num)
-                            output_func('Player {0} doubles down and gets {1} for a count of {2}'.format(player, card, count))
-                            done = True
+                            try:
+                                dealer.bank.withdraw(player, dealer.hand(player, hand_num).bet)
+                                dealer.bank.deposit(0, dealer.hand(player, hand_num).bet)
+                                dealer.hand(player, hand_num).bet *= 2
+                                card, count = dealer.hit(player, hand_num)
+                                output_func('Player {0} doubles down and gets {1} for a count of {2}'.format(player, card, count))
+                                done = True
+                            except InsufficientFunds:
+                                output_func('Player {0} doesn\'t have the cash for that!'.format(player))
+                                continue
                         elif ans == 'hit':
                             card, count = dealer.hit(player, hand_num)
                             output_func('Player {0} got {1} for a count of {2}'.format(player, card, count))
@@ -246,30 +303,58 @@ Pass input_func and output_func with appropriate vectors for other implementatio
 
         if dealer_count > 21:
             output_func('Dealer busted!')
-            for player in range(1, n+1):
+            for player in players:
                 if dealer.num_hands(player) == 1:
                     if dealer.count(player) <= 21:
                         winners.append(player)
+                        if dealer.count(player) == 21 and len(dealer.hand(player)) == 2:
+                            dealer.bank.deposit(player, 2.5*dealer.hand(player).bet)
+                            dealer.bank.withdraw(0, 2.5*dealer.hand(player).bet)
+                        else:
+                            dealer.bank.deposit(player, 2*dealer.hand(player).bet)
+                            dealer.bank.withdraw(0, 2*dealer.hand(player).bet)
                 else:
                     for hand_num in range(dealer.num_hands(player)):
                         if dealer.count(player, hand_num) <= 21:
                             winners.append('{0} {1} hand'.format(player, ordinal[hand_num + 1]))
+                            if dealer.count(player, hand_num) == 21 and len(dealer.hand(player, hand_num)) == 2:
+                                dealer.bank.withdraw(0, 2.5*dealer.hand(player, hand_num).bet)
+                                dealer.bank.deposit(player, 2.5*dealer.hand(player, hand_num).bet)
+                            else: 
+                                dealer.bank.withdraw(0, 2*dealer.hand(player, hand_num).bet)
+                                dealer.bank.deposit(player, 2*dealer.hand(player, hand_num).bet)
         else:
             output_func('Dealer standing at {0}'.format(dealer_count))
-            for player in range(1, n+1):
+            for player in players:
                 if dealer.num_hands(player) == 1:
                     c = dealer.count(player)
                     if c <= 21 and c > dealer_count:
                         winners.append(player)
+                        if c == 21 and len(dealer.hand(player)) == 2:
+                            dealer.bank.withdraw(0, 2.5*dealer.hand(player).bet)
+                            dealer.bank.deposit(player, 2.5*dealer.hand(player).bet)
+                        else:
+                            dealer.bank.withdraw(0, 2*dealer.hand(player).bet)
+                            dealer.bank.deposit(player, 2*dealer.hand(player).bet)
                     elif c == dealer_count:
                         pushers.append(player)
+                        dealer.bank.withdraw(0, dealer.hand(player).bet)
+                        dealer.bank.deposit(player, dealer.hand(player).bet)
                 else:
                     for hand_num in range(dealer.num_hands(player)):
                         c = dealer.count(player, hand_num)
                         if  c <= 21 and c > dealer_count:
                             winners.append('{0} {1} hand'.format(player, ordinal[hand_num + 1]))
+                            if c == 21 and len(dealer.hand(player, hand_num)) == 2:
+                                dealer.bank.withdraw(0, 2.5*dealer.hand(player, hand_num).bet)
+                                dealer.bank.deposit(player, 2.5*dealer.hand(player, hand_num).bet)
+                            else:
+                                dealer.bank.withdraw(0, 2*dealer.hand(player, hand_num).bet)
+                                dealer.bank.deposit(player, 2*dealer.hand(player, hand_num).bet)
                         elif c == dealer_count:
                             pushers.append('{0} {1} hand'.format(player, ordinal[hand_num + 1]))
+                            dealer.bank.withdraw(0, dealer.hand(player, hand_num).bet)
+                            dealer.bank.deposit(player, dealer.hand(player, hand_num).bet)
 
 
         if not winners and not pushers:
@@ -286,6 +371,16 @@ Pass input_func and output_func with appropriate vectors for other implementatio
 
         output_func("Play again?")
         keep_playing = read_answer(['yes', 'no', 'quit'], input_func=input_func, output_func=output_func)
+    house_net = dealer.bank.balance(0) - dealer.initial_balance*100
+
+    if abs(house_net) > 0.01:
+        output_func('House is {0} ${1:.2f}'.format('down' if house_net < 0 else 'up', abs(house_net)))
+    else:
+        output_func('House broke even')
+
+    for player in range(1, n+1):
+        output_func('Player {0} has a balance of ${1:.2f}'.format(player, dealer.bank.balance(player)))
+
 
 if __name__ == '__main__':
     play()
