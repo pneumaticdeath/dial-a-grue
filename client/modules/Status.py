7c568bfeabda779f432ccd300d3f6be4fd3e02da
Mitch Patenaude <dialagrue@gmail.com>
2018-04-22 05:51:07 +0000

    Status module will report on battery status

---
+++ client/modules/Status.py
@@ -0,0 +1,52 @@
+# vim: ai sw=4 expandtab:
+import logging
+import re
+import subprocess
+
+WORDS = [ 'CHECK', 'STATUS'  ]
+INSTANCE_WORDS = WORDS
+
+PRIORITY = 50
+
+def handle(text, mic, profile):
+    """
+    Check battery status
+    """
+
+    def output(string):
+        print(string)
+        mic.say(string)
+
+    output('Checking battery status')
+
+    status_output = subprocess.check_output('/usr/bin/battery.sh')
+    status = {}
+    for kvp in status_output.split('\n'):
+        if kvp.strip():
+            key, value = [s.strip() for s in kvp.split('=')]
+            print('{0}={1}'.format(key,value))
+            status[key] = value
+
+    if status.get('BAT_EXIST') == '1':
+        if status['CHARG_IND'] == '1':
+            charge_current = re.sub('mA$', ' mili-amps', status['Battery charge current'])
+            output('Battery is charging at {0}'.format(charge_current))
+        else:
+            discharge_current = re.sub('mA$', ' mili-amps', status['Battery discharge current'])
+            output('Battery is discharging at {0}'.format(discharge_current))
+        voltage = re.sub('mV$', ' mili-volts', status['Battery voltage'])
+        output('Battery voltage is {0}'.format(voltage))
+    else:
+        output('Battery not present.')
+
+    temp = re.sub('c$', ' degrees celsius', status['Internal temperature'])
+    output('Temperature is {0}.'.format(temp))
+
+    logging.info('Done checking batter status')
+
+def isValid(text):
+    """
+    Responds to the phrase "Check Status"
+    """
+
+    return bool(re.search(r'\bstatus\b', text, re.IGNORECASE))
