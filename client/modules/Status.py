dfac046fc3e22189e8cedbf7761b45642e70a0b3
Mitch Patenaude <dialagrue@gmail.com>
2018-05-18 22:13:54 +0000

    Reworked phone status to be platform independent

--- client/modules/Status.py
+++ client/modules/Status.py
@@ -1,5 +1,6 @@
 # vim: ai sw=4 expandtab:
 import logging
+import os
 import re
 import subprocess
 
@@ -20,29 +21,9 @@ def handle(text, mic, profile):
 
     output('Checking phone status')
 
-    status_output = subprocess.check_output('/usr/bin/battery.sh')
-    status = {}
-    for kvp in status_output.split('\n'):
-        if kvp.strip():
-            key, value = [s.strip() for s in kvp.split('=')]
-            # print('{0}={1}'.format(key,value))
-            status[key] = value
-
-    if status.get('BAT_EXIST') == '1':
-        if status['CHARG_IND'] == '1':
-            charge_current = re.sub('mA$', ' mili-amps', status['Battery charge current'])
-            output('Battery is charging at {0}'.format(charge_current))
-        else:
-            discharge_current = re.sub('mA$', ' mili-amps', status['Battery discharge current'])
-            output('Battery is discharging at {0}'.format(discharge_current))
-        voltage = re.sub('mV$', ' mili-volts', status['Battery voltage'])
-        output('Battery voltage is {0}'.format(voltage))
-    else:
-        output('Battery not present.')
-
-    temp = re.sub('c$', ' degrees celsius', status['Internal temperature'])
-    output('Temperature is {0}.'.format(temp))
-
+    status_output = subprocess.check_output('../bin/status')
+    for line in status_output.split('\n'):
+        output(line)
     logging.info('Done checking batter status')
 
 def isValid(text):
