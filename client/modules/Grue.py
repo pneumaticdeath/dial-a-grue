7f6737ffcdba01971b95c22c66e3748e1db1d9a5
Mitch Patenaude <dialagrue@gmail.com>
2020-06-21 15:40:43 -0700

    Split complex background command handing into separate util class so that tts can use it as well

--- client/modules/Grue.py
+++ client/modules/Grue.py
@@ -6,6 +6,7 @@ import pty
 import re
 import time
 from client import jasperpath
+from client.utils.run_while import run_while
 
 
 WORDS = ['GRUE']
@@ -18,43 +19,8 @@ def handle(text, mic, profile):
     # Easter egg
     phone = mic.phone
     mp3file = os.path.join(jasperpath.DATA_PATH, 'pitch_dark.mp3')
-    pid, fd = pty.fork()
-    if pid == 0:
-        args = ['/usr/bin/mpg321', mp3file]
-        time.sleep(0.5)
-        os.execv(args[0], args)
-    elif pid > 0:
-        logging.info('Forked pid {0}'.format(pid))
-        logger.debug(os.read(fd, 3000))
-        exited = False
-        while phone.off_hook():
-            time.sleep(0.1)
-            try:
-                stat_pid, status = os.waitpid(pid, os.WNOHANG)
-                if stat_pid == pid and os.WIFEXITED(status):
-                    exited = True
-                    if os.WEXITSTATUS(status) == 0:
-                        logger.info('mpg321 exited normally')
-                        break
-                    else:
-                        logger.warn('mpg321 exited with status {}'.format(os.WEXITSTATUS(status)))
-            except OSError as e:
-                logger.error('Error checking on pid {}: {}'.format(pid, str(e)))
-                break
-        if not exited:
-            logger.debug('Trying to kill pid {}'.format(pid))
-            try:
-                os.kill(pid, 1)
-                wait_pid, status = os.wait()
-                if wait_pid == pid:
-                    logger.debug('pid {0} exited with status {1}'.format(wait_pid, os.WEXITSTATUS(status)))
-                else:
-                    logger.warning('wait returned PID {0} when expecting {1} (status {2})'
-                                   .format(wait_pid, pid, status))
-            except OSError:
-                logger.warn('Got error when trying to kill child process')
-    else:
-        logger.warning('pty.fork returned status {0}'.format(pid))
+    args = ['/usr/bin/mpg321', mp3file]
+    run_while(phone.off_hook, args[0], args)
 
 def isValid(text):
     logger.debug('Heard "{0}"'.format(text))
---
+++ client/utils/run_while.py
@@ -0,0 +1,48 @@
+import os
+import logging
+import pty
+import time
+
+logger = logging.getLogger(__name__)
+
+def run_while(cond, cmd, args):
+    exited = False
+    pid = None
+
+    try:
+        pid, fd = pty.fork()
+        if pid == 0:
+            time.sleep(0.5)
+            os.execv(cmd, args)
+        elif pid < 0:
+            logger.warning('pty.fork() returned status {0}'.format(pid))
+        else:
+            logger.info('Ran cmd "{0}" in pid {1}'.format(' '.join(args), pid))
+            logger.debug(os.read(fd, 10240))
+            while cond():
+                time.sleep(0.1)
+                stat_pid, status = os.waitpid(pid, os.WNOHANG)
+                if stat_pid == pid and os.WIFEXITED(status):
+                    exited = True
+                    if os.WEXITSTATUS(status) == 0:
+                        logger.info('{0} exited nomally.'.format(cmd))
+                    else:
+                        logger.warning('pid {0} with cmd {1} and args "{2}" exited with status {3}.' 
+                                       .format(stat_pid, cmd, ' '.join(args), os.WEXITSTATUS(status)))
+                    pid = None
+                    break
+            if not cond():
+                logger.debug('Fell out of loop because condition no longer met')
+                
+            if pid is not None and not exited:
+                logger.debug('Trying to kill pid {0}'.format(pid))
+                os.kill(pid, 1)
+                wait_pid, status = os.wait()
+                if wait_pid == pid:
+                    logger.debug('pid {0} exited with status {1}'.format(wait_pid, os.WEXITSTATUS(status)))
+                else:
+                    logger.warning('Wait returned pid {0} when expecting {1} (status {2})'
+                                   .format(wait_pid, pid, status))
+    except OSError as e:
+        logger.warning('Got OSError "{0}"'.str(e))
+
